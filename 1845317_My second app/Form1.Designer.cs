72b9c0c45f5a845163ecf447b1df35c37a440f40
MOHAMMED MOHAMMED ALTOWAIJRI <1845317@stu.kau.edu.sa>
2020-06-14 00:05:12 +0300

    Radio code+ Design

--- 1845317_My second app/Form1.Designer.cs
+++ 1845317_My second app/Form1.Designer.cs
@@ -38,10 +38,10 @@
             this.button2 = new System.Windows.Forms.Button();
             this.BtnExit = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.btnRadio = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -143,6 +143,26 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Basic Controls";
             // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button3.Location = new System.Drawing.Point(85, 123);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 45);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Combo";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.Location = new System.Drawing.Point(158, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Check Box";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // btnRadio
             // 
             this.btnRadio.BackColor = System.Drawing.SystemColors.ActiveBorder;
@@ -163,26 +183,6 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.button1.Location = new System.Drawing.Point(158, 40);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(144, 38);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Check Box";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.button3.Location = new System.Drawing.Point(85, 123);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(144, 45);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Combo";
-            this.button3.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
